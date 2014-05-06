52e3c57c55978a2f60a5ac358e4e55ea8057caa7
Helbert Rios <helbertrios@gmail.com>
2014-05-06 12:52:58 -0300

    Teste.

---
+++ src/main/java/github/com/helbertrios/demo/javase/sobrecarga/ClasseSuporte.java
@@ -0,0 +1,28 @@
+package github.com.helbertrios.demo.javase.sobrecarga;
+
+public class ClasseSuporte {
+
+}
+
+class B {
+
+}
+
+class B1 extends B {
+
+}
+
+class B11 extends B1 {
+}
+
+class B12 extends B1 {
+}
+
+class B2 extends B {
+}
+
+class B21 extends B2 {
+}
+
+class B22 extends B2 {
+}
